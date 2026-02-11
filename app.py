b30fee6ae212c6ca893f71f136fefdf8ec6f0418
loke-netizen <codelearning2846@gmail.com>
2026-02-11 12:59:57 +0530

    Updated code

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import request
-from flask import render_template
-from flask import Flask
+from flask import redirect
+from flask import Flask,request,render_template
+
 app=Flask(__name__)
 
-@app.route("/")
-@app.route("/home")
+
+@app.route("/",methods=["POST","GET"]    )
+@app.route("/home",methods=["POST","GET"])
 def homepage():
-    return render_template("index.html")
-@app.route("/verify",methods=["POST"])
-def verifypage():
-    name=request.form.get("username")
-    email=request.form.get("email")
-    id=request.form.get("id")
-    return render_template("success.html",user=name,mail=email,password=id)
+        returning=""
+        if request.method=="POST":
+            name=request.form.get("username")
+            email=request.form.get("email")
+            id=request.form.get("id")
+            if name=="admin" and email=="code" and id=="1234":
+                return redirect("/verify/"+ name+"/"+id)
+            else:
+                returning="wrong check"
+                
+        return render_template("index.html",returning=returning)
+
+@app.route("/verify/<name>/<id>")
+def verifypage(name,id):
+    return render_template("success.html",user=name,id=id)
+
 if __name__=="__main__":
     app.run(debug=True)
